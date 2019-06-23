16df4ecc1a91b457e72f6ca78ad396aa8e233394
Villons96 <giacomo.villa.mi@gmail.com>
2019-06-23 19:54:44 +0200

    added plot about confusion matrix

--- Code/ModelRating.py
+++ Code/ModelRating.py
@@ -3,7 +3,10 @@ import numpy as np
 from math import floor
 from hmmlearn import hmm
 from sklearn.metrics import classification_report, accuracy_score
-import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+import matplotlib.pyplot as plotly
+import plotly.io as pio
 
 
 
@@ -135,6 +138,8 @@ def modelRating(model, actionObserved, correctActivity, matrixSize, targetActivi
 
     confusionMatrix = np.zeros((matrixSize, matrixSize))
 
+    # plotConfusionMatrix(correctActivity[0], posteriors, targetActivityName, False, "Confusion Matrix")
+
     cont = 0
     for activity in range(0, len(correctActivity[0])):
         if posteriors[activity] != correctActivity[0][activity]:
@@ -162,4 +167,51 @@ def modelRating(model, actionObserved, correctActivity, matrixSize, targetActivi
 # ['0: BasinPIRBathroom', 1: 'BedPressureBedroom', 2: 'CabinetMagneticBathroom', 
 #  3: 'CooktopPIRKitchen', 4: 'CupboardMagneticKitchen', 5: 'FridgeMagneticKitchen', 
 #  6: 'MaindoorMagneticEntrance', 7: 'MicrowaveElectricKitchen', 8: 'SeatPressureLiving', 
-#  9: 'ShowerPIRBathroom', 10: 'ToasterElectricKitchen', 11: 'ToiletFlushBathroom']
+#  9: 'ShowerPIRBathroom', 10: 'ToasterElectricKitchen', 11: 'ToiletFlushBathroom'] plt.cm.Blues
+
+def plotConfusionMatrix(y_true, y_pred, classes, normalize, title, cmap= plotly.cm.get_cmap("Blues")):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    # classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plotly.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap= cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plotly.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+
+    plotly.show(ax)
+    
--- Code/dash_house.py
+++ Code/dash_house.py
@@ -7,17 +7,26 @@ import os
 import plotly.graph_objs as go
 import dash_dangerously_set_inner_html
 
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 app.layout = html.Div([
-  html.H1('Hello Dash', style={
-        'textAlign': 'center'})
-
+  html.H1('Mother Sensor', style={
+        'textAlign': 'center'}),
+  html.Div([html.Button('O', id='seat')], style = {'height': 38, 'width': 66, 'position':'relative', 'top':450, 'left':850}),
+  html.Div([html.Button('O', id='cook')], style = {'height': 38, 'width': 66, 'position':'relative', 'top':433, 'left':1075})
 ], style={"height" : "100vh", "background-image": 'url("https://i.ibb.co/f8HwzyQ/wall.jpg")', 'background-repeat': 'no-repeat', 'background-position': 'center'})
 
+
+'''                              'layout': {
+                                    'height': 800,
+                                    'margin': {
+                                        'l': 10, 'b': 20, 't': 0, 'r': 0
+                                    }'''
 
 if __name__ == '__main__':
     app.run_server(debug=True)
